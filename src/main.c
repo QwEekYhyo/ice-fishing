16211cccfc6dab798422401d547c66b0be78e214
Logan Lucas <logan.lucas83@gmail.com>
2025-02-15 18:12:34 +0100

    do not store window and renderer in global variables

--- src/main.c
+++ src/main.c
@@ -3,12 +3,15 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_timer.h>
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 
-static SDL_Window* window     = NULL;
-static SDL_Renderer* renderer = NULL;
+typedef struct {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+} AppState;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
     SDL_SetAppMetadata("Ice Fishing", "0.1", "fr.puceaulytech.ice-fishing");
@@ -18,13 +21,18 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
         return SDL_APP_FAILURE;
     }
 
+    AppState* as = (AppState*) SDL_calloc(1, sizeof(AppState));
+    if (!as) return SDL_APP_FAILURE;
+
+    *appstate = as;
+
     if (!SDL_CreateWindowAndRenderer(
             "ice-fishing",
             WINDOW_WIDTH,
             WINDOW_HEIGHT,
             0,
-            &window,
-            &renderer
+            &as->window,
+            &as->renderer
         )) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -40,20 +48,30 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 }
 
 SDL_AppResult SDL_AppIterate(void* appstate) {
-    SDL_SetRenderDrawColor(renderer, 33, 33, 33, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
+    AppState* as = (AppState*) appstate;
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
+    SDL_SetRenderDrawColor(as->renderer, 33, 33, 33, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(as->renderer);
+
+    SDL_SetRenderDrawColor(as->renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
     SDL_FRect rect;
     rect.x = (WINDOW_WIDTH / 2.0) - 25;
     rect.y = (WINDOW_HEIGHT / 2.0) - 25;
     rect.w = 50;
     rect.h = 50;
-    SDL_RenderFillRect(renderer, &rect);
+    SDL_RenderFillRect(as->renderer, &rect);
 
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(as->renderer);
 
+    SDL_Delay(100);
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void* appstate, SDL_AppResult result) {}
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+    if (appstate == NULL) return;
+
+    AppState* as = (AppState*) appstate;
+    SDL_DestroyRenderer(as->renderer);
+    SDL_DestroyWindow(as->window);
+    SDL_free(as);
+}
