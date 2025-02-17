ebca1204fa257031014e732ef614d82a75d7c30a
Logan Lucas <logan.lucas83@gmail.com>
2025-02-17 12:25:05 +0100

    ensure all fishes are dead when the game starts

--- src/game_logic.c
+++ src/game_logic.c
@@ -1,3 +1,8 @@
+#include <fish/fish.h>
 #include <game_logic.h>
 
-void init_game(GameContext* ctx) {}
+void init_game(GameContext* ctx) {
+    for (unsigned int i = 0; i < MAX_FISHES; i++) {
+        ctx->fishes[i].state = DEAD;
+    }
+}
