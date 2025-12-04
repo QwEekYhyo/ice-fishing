5d7450ac72d578c54e1936ffb727e6ea11518b27
Logan Lucas <logan.lucas83@gmail.com>
2025-12-04 14:34:59 +0100

    refactor fish spawning

--- src/fish/fish.c
+++ src/fish/fish.c
@@ -1,3 +1,4 @@
+#include <SDL3/SDL_log.h>
 #include <SDL3/SDL_stdinc.h>
 
 #include <common_defs.h>
@@ -15,39 +16,43 @@ void move_fish(Fish* fish, unsigned long delta_time) {
         fish->state = DEAD;
 }
 
+#define FISH_TYPE_NUMBER 2
+typedef enum {
+    FISHTYPE_NORMAL = 0,
+    FISHTYPE_UPDOWN = 1,
+} FishType;
+
+// TODO: different probabilities for each type of fish
+typedef struct {
+    size_t size;
+    void (*init)(Fish*);
+} FishDesc;
+
+static const FishDesc FISH_DESCS[FISH_TYPE_NUMBER] = {
+    [FISHTYPE_NORMAL] = { sizeof(NormalFish), normal_fish_new },
+    [FISHTYPE_UPDOWN] = { sizeof(UpDownFish), up_down_fish_new },
+};
+
 Fish* spawn_fish(Fish* fish) {
     const float speed = (SDL_randf() * 2.0) - 1.0;
 
-    static const Uint8 FISH_TYPE_NUMBER = 2;
-    const Uint8 random_fish_type        = SDL_rand(FISH_TYPE_NUMBER);
+    const Uint8 random_type = SDL_rand(FISH_TYPE_NUMBER);
+    const FishDesc* fish_desc = &FISH_DESCS[random_type];
 
-    // TODO: different probabilities for each type of fish
-    size_t size;
-    switch (random_fish_type) {
-    default:
+    void* new_memory = SDL_realloc(fish, fish_desc->size);
+    if (!new_memory) {
+        SDL_Log("Failed to realloc fish");
         return fish;
-    case 0:
-        size = sizeof(NormalFish);
-        break;
-    case 1:
-        size = sizeof(UpDownFish);
-        break;
     }
-    fish = SDL_realloc(fish, size);
-
-    switch (random_fish_type) {
-    case 0:
-        normal_fish_new(fish);
-        break;
-    case 1:
-        up_down_fish_new(fish);
-        break;
-    }
+    fish = new_memory;
 
     fish->state = ALIVE;
-    fish->x     = speed >= 0 ? -fish->w : WINDOW_WIDTH + fish->w;
     fish->y     = SDL_rand(WINDOW_HEIGHT - WATER_Y - 70) + WATER_Y + 10;
-    fish->speed = speed != 0 ? speed : 0.1f;
+    fish->speed = speed != 0 ? speed : 0.1f; // Security just in case
+
+    fish_desc->init(fish);
+
+    fish->x = speed >= 0 ? -fish->w : WINDOW_WIDTH + fish->w;
 
     return fish;
 }
